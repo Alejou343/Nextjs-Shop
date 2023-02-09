03bd34f99c0f8c5a5af0eeb84df7c60894af9de1
Alejandro Alvarez <alejandro.auribe1@gmail.com>
2023-02-09 13:02:30 -0500

    Implementación de la PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['imageio.forbes.com', 'api.lorem.space'],
   },
-}
+};
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
